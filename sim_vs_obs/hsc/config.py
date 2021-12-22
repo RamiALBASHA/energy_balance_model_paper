5a4110db5a11d0bd911a1009e61912ab209aba9a
Rami <rami.albacha@yahoo.com>
2021-12-22 17:30:55 +0100

    add irradiance params

--- sim_vs_obs/hsc/config.py
+++ sim_vs_obs/hsc/config.py
@@ -67,3 +67,16 @@ class ParamsInfo:
             fig.savefig('stomatal_sensitivity_fit.png')
 
         self.psi_half_aperture = popt[0]
+
+
+class ParamsIrradiance(Enum):
+    leaf_reflectance = 0.08
+    leaf_transmittance = 0.07
+    leaves_to_sun_average_projection = 0.5
+    sky_sectors_number = 3
+    sky_type = 'soc'
+    canopy_reflectance_to_diffuse_irradiance = 0.057
+
+    @classmethod
+    def to_dict(cls):
+        return {member.name: member.value for member in cls}
