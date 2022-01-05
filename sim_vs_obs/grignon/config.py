6740ff23fc4b5dfd8a866330dfbe358ce9d261ce
Rami <rami.albacha@yahoo.com>
2022-01-05 18:15:51 +0100

    add PathInfos and WeatherStation classes

--- sim_vs_obs/grignon/config.py
+++ sim_vs_obs/grignon/config.py
@@ -1,4 +1,9 @@
 from enum import Enum
+from pathlib import Path
+
+
+class PathInfos(Enum):
+    source_fmt = Path(__file__).parents[2] / 'sources/grignon/data_fmt'
 
 
 class ParamsGapFract2Gai(Enum):
@@ -6,3 +11,9 @@ class ParamsGapFract2Gai(Enum):
     wheat = -0.824
     """Baret et al. (2010, doi:10.1016/j.agrformet.2010.04.011)
     """
+
+
+class WeatherStation(Enum):
+    latitude = 48.838
+    longitude = 1.953
+    altitude = 125
