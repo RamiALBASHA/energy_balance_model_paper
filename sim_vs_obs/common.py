55c268b8fcd20f86e6d3ff1268f6f24a79735143
Rami <rami.albacha@yahoo.com>
2022-02-02 13:19:04 +0100

    add ParamsEnergyBalanceBase

--- sim_vs_obs/common.py
+++ sim_vs_obs/common.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 from crop_irradiance.uniform_crops import inputs, params, shoot
 
 from sim_vs_obs.maricopa_hsc.config import ParamsIrradiance
@@ -41,3 +43,32 @@ def calc_absorbed_irradiance(
         {-1: {'lumped': (1. - soil_albedo) * non_absorbed_par_by_vegetation}})
 
     return absorbed_par_irradiance, canopy
+
+
+class ParamsEnergyBalanceBase(Enum):
+    stomatal_sensibility = {
+        "leuning": {"d_0": 7},
+        "misson": {"psi_half_aperture": -1, "steepness": 2}}
+    soil_aerodynamic_resistance_shape_parameter = 2.5
+    soil_roughness_length_for_momentum = 0.0125
+    leaf_characteristic_length = 0.01
+    leaf_boundary_layer_shape_parameter = 0.01
+    wind_speed_extinction_coef = 0.5
+    maximum_stomatal_conductance = 80.0
+    residual_stomatal_conductance = 1.0
+    diffuse_extinction_coef = None
+    leaf_scattering_coefficient = None
+    leaf_emissivity = None
+    soil_emissivity = None
+    absorbed_par_50 = 43
+    soil_resistance_to_vapor_shape_parameter_1 = 8.206
+    soil_resistance_to_vapor_shape_parameter_2 = 4.255
+    step_fraction = 0.5
+    acceptable_temperature_error = 0.02
+    maximum_iteration_number = 50
+    stomatal_density_factor = 1
+    atmospheric_emissivity_model = 'monteith_2013'
+
+    @classmethod
+    def to_dict(cls):
+        return {name: member.value for name, member in cls.__members__.items()}
