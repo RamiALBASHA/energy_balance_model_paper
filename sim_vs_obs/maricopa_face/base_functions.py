986710fd6fac1fb4db6a3b9661607e9048951e29
Rami <rami.albacha@yahoo.com>
2022-02-02 11:46:52 +0100

    add build_area_profile()

--- sim_vs_obs/maricopa_face/base_functions.py
+++ sim_vs_obs/maricopa_face/base_functions.py
@@ -1,4 +1,4 @@
-from pandas import DataFrame, read_excel
+from pandas import DataFrame, read_excel, Series
 
 from sim_vs_obs.maricopa_face.config import ExpIdInfos, PathInfos
 
@@ -8,3 +8,10 @@ def get_area_data() -> DataFrame:
     df = read_excel(path_obs, engine='odf', sheet_name='Obs_daily_Avg_over_Reps')
     plot_ids = ExpIdInfos.get_studied_plot_ids()
     return df[df['TRNO'].isin(plot_ids) & ~df['LNUM'].isna()]
+
+
+def build_area_profile(treatment_data: Series) -> dict:
+    number_leaf = int(treatment_data['LNUM'])
+    total_leaf_area_index = treatment_data['LAID']
+    total_stem_area_index = treatment_data['SAID']
+    return {k + 1: (total_stem_area_index + total_leaf_area_index) / number_leaf for k in range(number_leaf)}
