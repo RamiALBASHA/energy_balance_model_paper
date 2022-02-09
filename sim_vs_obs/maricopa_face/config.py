79926b47b49a77aea534348e86b91f8d54f2f655
Rami <rami.albacha@yahoo.com>
2022-02-09 09:38:51 +0100

    add year to ExpIdInfos member infos

--- sim_vs_obs/maricopa_face/config.py
+++ sim_vs_obs/maricopa_face/config.py
@@ -11,22 +11,22 @@ class PathInfos(Enum):
 
 
 class ExpIdInfos(Enum):
-    exp_901 = ('Dry', 'High N', 'Ambient CO2')
-    exp_902 = ('Wet', 'High N', 'Ambient CO2')
-    exp_903 = ('Dry', 'High N', 'Elevated CO2')
-    exp_904 = ('Wet', 'High N', 'Elevated CO2')
-    exp_905 = ('Dry', 'High N', 'Ambient CO2')
-    exp_906 = ('Wet', 'High N', 'Ambient CO2')
-    exp_907 = ('Dry', 'High N', 'Elevated CO2')
-    exp_908 = ('Wet', 'High N', 'Elevated CO2')
-    exp_909 = ('Wet', 'Low N', 'Ambient CO2')
-    exp_910 = ('Wet', 'High N', 'Ambient CO2')
-    exp_911 = ('Wet', 'Low N', 'Elevated CO2')
-    exp_912 = ('Wet', 'High N', 'Elevated CO2')
-    exp_913 = ('Wet', 'Low N', 'Ambient CO2')
-    exp_914 = ('Wet', 'High N', 'Ambient CO2')
-    exp_915 = ('Wet', 'Low N', 'Elevated CO2')
-    exp_916 = ('Wet', 'High N', 'Elevated CO2')
+    exp_901 = ('Dry', 'High N', 'Ambient CO2', '1993')
+    exp_902 = ('Wet', 'High N', 'Ambient CO2', '1993')
+    exp_903 = ('Dry', 'High N', 'Elevated CO2', '1993')
+    exp_904 = ('Wet', 'High N', 'Elevated CO2', '1993')
+    exp_905 = ('Dry', 'High N', 'Ambient CO2', '1994')
+    exp_906 = ('Wet', 'High N', 'Ambient CO2', '1994')
+    exp_907 = ('Dry', 'High N', 'Elevated CO2', '1994')
+    exp_908 = ('Wet', 'High N', 'Elevated CO2', '1994')
+    exp_909 = ('Wet', 'Low N', 'Ambient CO2', '1996')
+    exp_910 = ('Wet', 'High N', 'Ambient CO2', '1996')
+    exp_911 = ('Wet', 'Low N', 'Elevated CO2', '1996')
+    exp_912 = ('Wet', 'High N', 'Elevated CO2', '1996')
+    exp_913 = ('Wet', 'Low N', 'Ambient CO2', '1997')
+    exp_914 = ('Wet', 'High N', 'Ambient CO2', '1997')
+    exp_915 = ('Wet', 'Low N', 'Elevated CO2', '1997')
+    exp_916 = ('Wet', 'High N', 'Elevated CO2', '1997')
 
     @classmethod
     def get_studied_plot_ids(cls):
