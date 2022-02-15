36e1982933c7c4d0ca0c4760730323a661af89d4
Rami <rami.albacha@yahoo.com>
2022-02-15 18:28:14 +0100

    add calc_mean()

--- utils/stats.py
+++ utils/stats.py
@@ -11,3 +11,7 @@ def calc_r2(vector_1: Vector, vector_2: Vector) -> float:
 
 def calc_rmse(vector_1: Vector, vector_2: Vector) -> float:
     return (sum([(x - y) ** 2 for x, y in zip(vector_1, vector_2)]) / len(vector_1)) ** 0.5
+
+
+def calc_mean(vector: Vector):
+    return sum(vector) / len(vector)
