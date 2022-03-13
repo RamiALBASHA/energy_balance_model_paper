69648ae81c393e16ecb07a1137dd13f30d9e4147
Rami <rami.albacha@yahoo.com>
2022-03-13 23:25:27 +0100

    innterpolate leaf area among dates

--- sim_vs_obs/maricopa_face/main.py
+++ sim_vs_obs/maricopa_face/main.py
@@ -14,42 +14,51 @@ if __name__ == '__main__':
     area_df = area_df[~area_df['LNUM'].isna()]
 
     sim_obs_dict = {k: {} for k in area_df['TRNO'].unique()}
-    for date_obs, row in area_df.iterrows():
-        treatment = int(row['TRNO'])
-        print(date_obs, treatment)
 
-        date_min, date_max = base_functions.get_date_bounds(
-            [weather_df[weather_df['DATE'].dt.year == date_obs.year]['DATE'], soil_df.index, heights[treatment].index])
+    for treatment in area_df['TRNO'].unique():
+        trt_area_df = base_functions.interpolate_area_df(df=area_df[area_df['TRNO'] == treatment])
 
-        if date_min <= date_obs <= date_max:
-            weather_at_date = base_functions.get_weather(raw_data=weather_df[weather_df['DATE'].dt.date == date_obs])
+        for date_obs, row in trt_area_df.iterrows():
+            print(date_obs, treatment)
 
-            gai_profile = base_functions.build_area_profile(treatment_data=row, is_bigleaf=SimInfos.is_bigleaf.value)
+            date_min, date_max = base_functions.get_date_bounds(
+                [weather_df[weather_df['DATE'].dt.year == date_obs.year]['DATE'],
+                 soil_df.index,
+                 heights[treatment].index])
 
-            soil_data = base_functions.calc_soil_moisture(raw_data=soil_df, treatment_id=treatment, date_obs=date_obs)
+            if date_min <= date_obs <= date_max:
+                weather_at_date = base_functions.get_weather(
+                    raw_data=weather_df[weather_df['DATE'].dt.date == date_obs])
 
-            canopy_height = heights[treatment].loc[date_obs, 'avg']
+                gai_profile = base_functions.build_area_profile(
+                    treatment_data=row, is_bigleaf=SimInfos.is_bigleaf.value)
 
-            for datetime_obs, weather_hourly in weather_at_date.iterrows():
-                eb_inputs, eb_params = base_functions.set_energy_balance_inputs(
-                    leaf_layers=gai_profile,
-                    is_lumped=False,
-                    weather_data=weather_hourly,
-                    canopy_height=canopy_height / 100.,
-                    soil_data=soil_data)
+                soil_data = base_functions.calc_soil_moisture(
+                    raw_data=soil_df, treatment_id=treatment, date_obs=date_obs)
 
-                solver = Solver(leaves_category=SimInfos.leaf_category.value,
-                                inputs_dict=eb_inputs,
-                                params_dict=eb_params)
-                solver.run(is_stability_considered=True)
+                canopy_height = heights[treatment].loc[date_obs, 'avg']
 
-                sim_obs_dict[treatment].update({
-                    datetime_obs: {
-                        'solver': solver,
-                        'obs_energy_balance': base_functions.get_obs_energy_balance(
-                            all_obs=obs_energy_balance, treatment_id=treatment, datetime_obs=datetime_obs),
-                        'obs_sunlit_shaded': base_functions.get_obs_sunlit_shaded_temperature(
-                            obs_df=obs_portable_infrared_obs_1993, treatment_id=treatment, datetime_obs=datetime_obs)
-                    }})
+                for datetime_obs, weather_hourly in weather_at_date.iterrows():
+                    eb_inputs, eb_params = base_functions.set_energy_balance_inputs(
+                        leaf_layers=gai_profile,
+                        is_lumped=False,
+                        weather_data=weather_hourly,
+                        canopy_height=canopy_height / 100.,
+                        soil_data=soil_data)
+
+                    solver = Solver(leaves_category=SimInfos.leaf_category.value,
+                                    inputs_dict=eb_inputs,
+                                    params_dict=eb_params)
+                    solver.run(is_stability_considered=True)
+
+                    sim_obs_dict[treatment].update({
+                        datetime_obs: {
+                            'solver': solver,
+                            'obs_energy_balance': base_functions.get_obs_energy_balance(
+                                all_obs=obs_energy_balance, treatment_id=treatment, datetime_obs=datetime_obs),
+                            'obs_sunlit_shaded': base_functions.get_obs_sunlit_shaded_temperature(
+                                obs_df=obs_portable_infrared_obs_1993, treatment_id=treatment,
+                                datetime_obs=datetime_obs)
+                        }})
 
     plots.plot_comparison_energy_balance(sim_obs=sim_obs_dict)
