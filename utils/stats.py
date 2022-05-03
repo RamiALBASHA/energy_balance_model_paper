c0ee5d8a29645f5e30a0e498eacc936f49b18ab4
Rami <rami.albacha@yahoo.com>
2022-05-03 15:38:01 +0200

    add Nash-Sutcliffe criteria funcs

--- utils/stats.py
+++ utils/stats.py
@@ -15,3 +15,18 @@ def calc_rmse(vector_1: Vector, vector_2: Vector) -> float:
 
 def calc_mean(vector: Vector):
     return sum(vector) / len(vector)
+
+
+def calc_nash_sutcliffe(sim: Vector, obs: Vector) -> float:
+    obs_mean = calc_mean(vector=obs)
+    sim_variance = []
+    obs_variance = []
+    for sim_value, obs_value in zip(sim, obs):
+        sim_variance.append((sim_value - obs_value) ** 2)
+        obs_variance.append((obs_value - obs_mean) ** 2)
+
+    return 1. - sum(sim_variance) / sum(obs_variance)
+
+
+def calc_normaized_nash_sutcliffe(sim: Vector, obs: Vector) -> float:
+    return 1. / (2. - calc_nash_sutcliffe(sim=sim, obs=obs))
