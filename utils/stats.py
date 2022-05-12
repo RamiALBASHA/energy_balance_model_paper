6cedb34f036aac6b0b65570ff7dbdbde23b47446
Rami <rami.albacha@yahoo.com>
2022-05-12 11:18:23 +0200

    add mean quared deviation components stats

--- utils/stats.py
+++ utils/stats.py
@@ -30,3 +30,57 @@ def calc_nash_sutcliffe(sim: Vector, obs: Vector) -> float:
 
 def calc_normaized_nash_sutcliffe(sim: Vector, obs: Vector) -> float:
     return 1. / (2. - calc_nash_sutcliffe(sim=sim, obs=obs))
+
+
+def calc_mean_squared_deviation_components(sim: Vector, obs: Vector) -> tuple[float, float, float]:
+    sb = calc_squared_bias(sim=sim, obs=obs)
+    nu = calc_nonunity_slope(sim=sim, obs=obs)
+    lc = calc_lack_of_correlation(sim=sim, obs=obs)
+    return sb, nu, lc
+
+
+def calc_mean_squared_deviation(sim: Vector, obs: Vector) -> float:
+    return sum([(v_sim - v_obs) ** 2 for v_sim, v_obs in zip(sim, obs)]) / len(sim)
+
+
+def calc_squared_bias(sim: Vector, obs: Vector) -> float:
+    return (calc_mean(sim) - calc_mean(obs)) ** 2
+
+
+def calc_nonunity_slope(sim: Vector, obs: Vector) -> float:
+    slope = calc_slope(sim=sim, obs=obs)
+    sim_mean = calc_mean(sim)
+    sim_variance = sum([(v - sim_mean) ** 2 for v in sim]) / len(sim)
+
+    return (1 - slope) ** 2 * sim_variance
+
+
+def calc_lack_of_correlation(sim: Vector, obs: Vector) -> float:
+    obs_mean = calc_mean(obs)
+    obs_variance = sum([(v - obs_mean) ** 2 for v in obs]) / len(obs)
+    r2 = calc_correlation(sim=sim, obs=obs)
+    # r2 = calc_r2(sim, obs)
+
+    return (1 - r2) * obs_variance
+
+
+def calc_slope(sim: Vector, obs: Vector) -> float:
+    sim_mean = calc_mean(sim)
+    obs_mean = calc_mean(obs)
+
+    numerator_ls = []
+    denominator_ls = []
+    for v_sim, v_obs in zip(sim, obs):
+        numerator_ls.append((v_sim - sim_mean) * (v_obs - obs_mean))
+        denominator_ls.append((v_sim - sim_mean) ** 2)
+    return sum(numerator_ls) / sum(denominator_ls)
+
+
+def calc_correlation(sim: Vector, obs: Vector) -> float:
+    sim_mean = calc_mean(sim)
+    obs_mean = calc_mean(obs)
+
+    sim_deviation_ls, obs_deviation_ls = zip(*[(v_sim - sim_mean, v_obs - obs_mean) for v_sim, v_obs in zip(sim, obs)])
+    covariance = sum([v_sim * v_obs for v_sim, v_obs in zip(sim_deviation_ls, obs_deviation_ls)]) ** 2
+    variance_product = sum([v ** 2 for v in sim_deviation_ls]) * sum([v ** 2 for v in obs_deviation_ls])
+    return covariance / variance_product
