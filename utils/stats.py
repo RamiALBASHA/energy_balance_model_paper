855cc1b51d0b8e804d8262d202b009b0bc19c2c0
Rami <rami.albacha@yahoo.com>
2022-05-12 15:06:42 +0200

    bugfix r2 value (raised to 2)

--- utils/stats.py
+++ utils/stats.py
@@ -6,7 +6,7 @@ Vector = Union[list, tuple, array]
 
 
 def calc_r2(vector_1: Vector, vector_2: Vector) -> float:
-    return corrcoef(array(vector_1), array(vector_2))[0, 1]
+    return corrcoef(array(vector_1), array(vector_2))[0, 1] ** 2
 
 
 def calc_rmse(vector_1: Vector, vector_2: Vector) -> float:
