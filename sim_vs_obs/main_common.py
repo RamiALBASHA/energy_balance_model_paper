c33a302027e3f6df8a936b353febb5a878a8929d
Rami <rami.albacha@yahoo.com>
2022-06-14 14:58:28 +0200

    format sim vs obs temperature fig

--- sim_vs_obs/main_common.py
+++ sim_vs_obs/main_common.py
@@ -20,7 +20,7 @@ EXPERIMENTS = {
 
 def add_1_1_line(ax: pyplot.Subplot, lims: dict, **kwargs):
     lims = [lims[s] for s in ('min', 'max')]
-    ax.plot(lims, lims, 'k--', label='1:1', **kwargs)
+    ax.plot(lims, lims, 'k--', **kwargs)
     pass
 
 
@@ -35,37 +35,52 @@ def plot_sim_vs_obs(path_source: Path, path_outputs: Path, is_corrected: bool = 
     vars_to_plot = ('temperature_canopy', 'delta_temperature_canopy')
     lims = {s: {'min': 0, 'max': 0} for s in vars_to_plot}
 
-    fig, axs = pyplot.subplots(ncols=len(EXPERIMENTS), figsize=(7.48, 4.8), nrows=2, sharex='row', sharey='row',
-                               gridspec_kw=dict(wspace=0, hspace=0.5))
+    fig, axs = pyplot.subplots(nrows=2, ncols=len(EXPERIMENTS), figsize=(19 / 2.54, 12 / 2.54),
+                               sharex='row', sharey='row', gridspec_kw=dict(wspace=0, hspace=0.5))
     for j, experiment in enumerate(EXPERIMENTS.keys()):
         dir_name = '_'.join((EXPERIMENTS[experiment][1], s_lumped))
         df = read_csv(path_source / experiment / 'outputs' / s_corrected / dir_name / 'results.csv')
-        axs[0, j].set_title(EXPERIMENTS[experiment][0], fontsize=10, pad=20)
+        axs[0, j].set_title(EXPERIMENTS[experiment][0], fontsize=10, pad=20, fontweight='bold')
         for i, var in enumerate(vars_to_plot):
             ax = axs[i, j]
+            for period in ('day', 'night'):
+                df_ = df[df['incident_par'] > 0] if period == 'day' else df[df['incident_par'] == 0]
+                sim_ = df_[f'{var}_sim']
+                obs_ = df_[f'{var}_obs']
+                par_ = df_['incident_par']
+                ax.scatter(obs_, sim_, c=par_, label=period, **plot_kwargs)
+
             sim = df[f'{var}_sim']
             obs = df[f'{var}_obs']
-            ax.scatter(obs, sim, c=df['incident_par'], **plot_kwargs)
             ax.text(0.05, 0.875, f"({ascii_lowercase[j + i * 4]})", transform=ax.transAxes)
             ax.text(0.95, 0.25, f"R² = {stats.calc_r2(sim, obs):.2f}", transform=ax.transAxes, **kwargs)
             ax.text(0.95, 0.15, f"RMSE = {stats.calc_rmse(sim, obs):.2f}", transform=ax.transAxes, **kwargs)
-            ax.text(0.95, 0.05, f"nNS = {stats.calc_nash_sutcliffe(sim, obs):.2f}", transform=ax.transAxes, **kwargs)
+            ax.text(0.95, 0.05, f"nNSE = {stats.calc_nash_sutcliffe(sim, obs):.2f}", transform=ax.transAxes, **kwargs)
             lims[var]['min'] = min(lims[var]['min'], min(sim.min(), obs.min()))
             lims[var]['max'] = max(lims[var]['max'], max(sim.max(), obs.max()))
             ax.set_aspect(aspect='equal')
 
     for i, s, var in zip(range(len(vars_to_plot)), ('', ' depression'), vars_to_plot):
         axs[i, 0].set_ylabel(f"Simulated canopy\ntemperature{s}\n{t_unit}")
-        axs[i, 1].set_xlabel(f'Observed canopy temperature{s} {t_unit}')
+        axs[i, 1].set_xlabel(f'Measured canopy temperature{s} {t_unit}')
         axs[i, 1].xaxis.set_label_coords(1, -0.21, transform=axs[i, 1].transAxes)
 
     for ax_t, ax_dt, experiment in zip(axs[0, :], axs[1, :], EXPERIMENTS.values()):
         add_1_1_line(ax_t, lims=lims['temperature_canopy'], linewidth=0.5)
         add_1_1_line(ax_dt, lims=lims['delta_temperature_canopy'], linewidth=0.5)
+        ax_t.tick_params(axis='both', which='major', labelsize=8)
+        ax_dt.tick_params(axis='both', which='major', labelsize=8)
+
+    axs[0, 0].xaxis.set_major_locator(ticker.MultipleLocator(10))
+    axs[1, 0].xaxis.set_major_locator(ticker.MultipleLocator(5))
+    axs[1, 0].yaxis.set_major_locator(ticker.MultipleLocator(5))
+
+    axs[0, 0].legend(fontsize=8, framealpha=0, loc=(0.05, 0.6), handlelength=0.25)
 
     fig.subplots_adjust(left=0.15, right=0.985, top=0.89, bottom=0.125)
 
     fig.savefig(path_outputs / f'sim_vs_obs_all_experiments_{s_corrected}_{s_lumped}.png')
+    pyplot.close('all')
     pass
 
 
