f78fbf198fb8763d74b5692446264e6e49dfcad9
Rami <rami.albacha@yahoo.com>
2022-06-14 17:04:51 +0200

    format sunlit shaded fig

--- sim_vs_obs/maricopa_face/plots.py
+++ sim_vs_obs/maricopa_face/plots.py
@@ -439,16 +439,34 @@ def plot_daily_dynamic(counter, date_obs, trt_id, gai, hours, par_inc, par_abs_v
     pass
 
 
-def plot_sim_obs_sunlit_shaded(res_all: dict, res_wet: dict, res_dry: dict, figure_dir: Path):
+def plot_sim_obs_sunlit_shaded(res_wet: dict, res_dry: dict, figure_dir: Path, is_delta_t: bool = False):
     pyplot.close()
     fig, axs = pyplot.subplots(ncols=2, figsize=(14 / 2.54, 9 / 2.54), sharex='all', sharey='all',
                                gridspec_kw=dict(wspace=0))
-    for res, label, c in (res_wet, 'Well watered', 'b'), (res_dry, 'Water deficit', 'r'):
-        for ax, (k, v) in zip(axs, res.items()):
-            ax.scatter(v['obs'], v['sim'], label=label, alpha=0.5)
+
+    data = dict(temperature_sunlit=dict(sim=[], obs=[]),
+                temperature_shaded=dict(sim=[], obs=[]))
+
+    if is_delta_t:
+        for res, label, c in (res_wet, 'Well watered', 'b'), (res_dry, 'Water deficit', 'r'):
+            for ax, s in zip(axs, data.keys()):
+                obs_dt, sim_dt = zip(*[(t_obs - t_air, t_sim - t_air) for t_obs, t_sim, t_air in
+                                       zip(res[s]['obs'], res[s]['sim'], res['temperature_air'])
+                                       if not any(isna([t_obs, t_sim, t_air]))])
+                data[s]['obs'] += obs_dt
+                data[s]['sim'] += sim_dt
+                ax.scatter(obs_dt, sim_dt, label=label, alpha=0.5)
+    else:
+        for res, label, c in (res_wet, 'Well watered', 'b'), (res_dry, 'Water deficit', 'r'):
+            for ax, s in zip(axs, data.keys()):
+                obs_t, sim_t = zip(*[(t_obs, t_sim) for t_obs, t_sim in zip(res[s]['obs'], res[s]['sim'])
+                                     if not any(isna([t_obs, t_sim]))])
+                data[s]['obs'] += obs_t
+                data[s]['sim'] += sim_t
+                ax.scatter(obs_t, sim_t, label=label, alpha=0.5)
 
     text_kwargs = dict(fontsize=8, ha='right')
-    for ax, (k, v), s in zip(axs, res_all.items(), ascii_lowercase):
+    for ax, (k, v), s in zip(axs, data.items(), ascii_lowercase):
         ax.text(0.05, 0.875, f'({s})', transform=ax.transAxes)
         obs_ls, sim_ls = v['obs'], v['sim']
         obs_ls, sim_ls = zip(*[(obs, sim) for obs, sim in zip(obs_ls, sim_ls) if not any(isna([obs, sim]))])
@@ -458,13 +476,14 @@ def plot_sim_obs_sunlit_shaded(res_all: dict, res_wet: dict, res_dry: dict, figu
                 transform=ax.transAxes, **text_kwargs)
         add_1_1_line(ax, linewidth=0.5)
         ax.set_aspect(aspect='equal', anchor='C')
+
     axs[0].legend(fontsize=8, loc='lower left')
     axs[0].set_ylabel('Simulated leaf temperature (°C)')
     axs[0].set_xlabel('Observed leaf temperature (°C)')
     axs[0].xaxis.set_label_coords(1.05, -0.15, transform=axs[0].transAxes)
     axs[0].yaxis.set_major_formatter(ticker.FormatStrFormatter('%d'))
     fig.tight_layout()
-    fig.savefig(figure_dir / 'sunlit_shaded.png')
+    fig.savefig(figure_dir / f"sunlit_shaded{'_dt' if is_delta_t else '_t'}.png")
     pyplot.close("all")
 
     pass
